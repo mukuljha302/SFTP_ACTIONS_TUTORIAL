8202a48b6b1d71c6993743420734c6161149607a
Jha <mukul.jha@accenture.com>
2024-03-19 16:25:15 +0530

    second commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+
+let Client = require('ssh2-sftp-client');
+
+const sftp = new Client();
+
+(async () => {
+
+  try {
+
+
+    // Connect to the SFTP server
+    await sftp.connect({
+      host: 'sftp://student116.files.com/files/',
+      port: 22,
+      username: 'mukuljha302@gmail.com',
+      password: 'Filessftp392@',
+    });
+
+    console.log('Connected to SFTP server');
+    // Your SFTP actions will go here
+
+  } catch (err) {
+    console.error('Error:', err.message);
+  } finally {
+    // Disconnect from the SFTP server after all actions
+    sftp.end();
+  }
+})();
